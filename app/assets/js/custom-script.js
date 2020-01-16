55f70e0e888ed7afa46f24d948cf2bdc35453d77
Naut de Jong <naut@MPs-Mac-mini.local>
2020-01-16 17:29:18 +0100

    frontend files

---
+++ app/assets/js/custom-script.js
@@ -0,0 +1,2 @@
+/* Custom script */
+$(document).ready(function() {});
--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -2,17 +2,17 @@
  * Load Bootstrap
  * Path: /node_modules/bootstrap/dist/js/
 !*/
-import 'bootstrap';
+// import 'bootstrap';
 
 /* Modular import */
 /* Required */
-// import 'bootstrap/js/src/util';
+import 'bootstrap/js/src/util';
 
 /* Optional */
 // import 'bootstrap/js/src/alert';
 // import 'bootstrap/js/src/button';
 // import 'bootstrap/js/src/carousel';
-// import 'bootstrap/js/src/collapse';
+import 'bootstrap/js/src/collapse';
 // import 'bootstrap/js/src/dropdown';
 // import 'bootstrap/js/src/modal';
 // import 'bootstrap/js/src/popover';
