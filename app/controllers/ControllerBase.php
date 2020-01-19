7e68a78081432262f634488d70de125f09234956
Naut de Jong <andejong@ctrl-home.nl>
2020-01-19 01:02:34 +0100

    use phalcon's asset manager

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -1,9 +1,22 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
-
+    public function onConstruct() {
+        $this
+            ->assets
+            ->collection('header')
+            ->addCSS('css/frontend.css')
+            ->setPrefix('http://'.$_SERVER['HTTP_HOST'].':8081'.DIRECTORY_SEPARATOR);
+        $this
+            ->assets
+            ->collection('footer')
+            ->addJs('/js/frontend.js')
+            ->addJs('/js/vendor.frontend.js')
+            ->addJs('/js/main.frontend.js')
+            ->setPrefix('http://'.$_SERVER['HTTP_HOST'].':8081'.DIRECTORY_SEPARATOR);
+    }
 }
